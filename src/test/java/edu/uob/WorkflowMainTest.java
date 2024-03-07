e28f4c759018ff95815787529ffdf989a3cfa479
mihirgandecha <mihirgandecha@outlook.com>
2024-03-07 15:14:47 +0000

    Restructure directories

---
+++ src/test/java/edu/uob/WorkflowMainTest.java
@@ -0,0 +1,12 @@
+package edu.uob;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoTest {
+
+    @Test
+    void demo() {
+        assertTrue(1 == 1);
+    }
+}
