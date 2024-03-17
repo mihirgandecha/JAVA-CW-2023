21b7beddd7aa195dba1d1f181a9c09fc8e35ff33
mihirgandecha <mihirgandecha@outlook.com>
2024-03-17 00:06:24 +0000

    @Serial Added

--- cw-db/src/main/java/edu/uob/DBParse/SyntaxException.java
+++ cw-db/src/main/java/edu/uob/DBParse/SyntaxException.java
@@ -1,6 +1,11 @@
 package edu.uob.DBParse;
 
+import java.io.Serial;
+
 public class SyntaxException extends Exception {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     public String errorTag;
     String errorMsg;
 
