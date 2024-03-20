35ef4f11a2676dd27cca9fbd0b4755c0102ed59f
Mihir Gandecha <109607930+mihirgandecha@users.noreply.github.com>
2024-03-20 21:12:09 +0000

    Update Alter.java: Return OK AlterCmd

--- cw-db/src/main/java/edu/uob/DBCmnd/Alter.java
+++ cw-db/src/main/java/edu/uob/DBCmnd/Alter.java
@@ -13,7 +13,7 @@ public class Alter extends Database implements DBCmnd {
 
     @Override
     public String execute(Parser p) throws SyntaxException, IOException {
-        return null;
+        return "[OK]";
     }
 
 }
