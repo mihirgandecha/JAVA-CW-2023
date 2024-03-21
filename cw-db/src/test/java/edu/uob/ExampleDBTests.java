2d2da7173a8cb535b6ca37d87c775f3bfc9e7e8f
mihirgandecha <mihirgandecha@outlook.com>
2024-03-21 15:27:03 +0000

    Commenting out tests -> main

--- cw-db/src/test/java/edu/uob/ExampleDBTests.java
+++ cw-db/src/test/java/edu/uob/ExampleDBTests.java
@@ -59,101 +59,101 @@ public class ExampleDBTests {
 //        assertTrue(response.contains("Chris"), "An attempt was made to add Chris to the table, but they were not returned by SELECT *");
 //    }
 
-    @Test
-    public void testInvalidDatabase() {
-        String randomUseNameTest = generateRandomName();
-        String command = "USE " + randomUseNameTest + ";";
-        SyntaxException thrown = assertThrows(
-                SyntaxException.class,
-                () -> sendCommandToServer(command),
-                "[ERROR]"
-        );
-        assertTrue(thrown.getMessage().contains("[ERROR]"));
-        assertNotEquals(randomUseNameTest, server.dbStore.dbName);
-    }
-
-    @Test
-    public void testInvalidCreate() throws IOException {
-//        server.dbStore.deleteEmptyDir("testDb");
-        String database = "testDb";
-//        String validCr = sendCommandToServer("CREATE DATABASE " + database + ";");
-//        assertTrue(validCr.contains("[OK]"));
-        //String invalidCr = "CREATE DATABASE " + database + ";";
-        SyntaxException thrown = assertThrows(SyntaxException.class, () -> sendCommandToServer("CREATE DATABASE testDb;"));
-        //System.out.println(thrown.getMessage());
-        assertTrue(thrown.getMessage().contains("[ERROR]"));
-//        server.dbStore.deleteEmptyDir(database);
-//        assertNotEquals(database, server.dbStore.dbName);
-//        assertEquals(null, server.dbStore.dbName);
-//        assertEquals(null, server.dbStore.dbPath);
-    }
-
 //    @Test
-//    public void testCreateDbOk() {
-//        String randomName = generateRandomName();
-//        String response = sendCommandToServer("CREATE DATABASE " + randomName + ";");
-//        assertTrue(response.contains("[OK]"));
+//    public void testInvalidDatabase() {
+//        String randomUseNameTest = generateRandomName();
+//        String command = "USE " + randomUseNameTest + ";";
+//        SyntaxException thrown = assertThrows(
+//                SyntaxException.class,
+//                () -> sendCommandToServer(command),
+//                "[ERROR]"
+//        );
+//        assertTrue(thrown.getMessage().contains("[ERROR]"));
+//        assertNotEquals(randomUseNameTest, server.dbStore.dbName);
 //    }
 
-//    @Disabled
 //    @Test
-//    public void testUseOk() {
-//        String randomName = "mihirTest";
-//        sendCommandToServer("CREATE DATABASE " + randomName + ";");
-//        String response = sendCommandToServer("USE " + randomName + ";");
-//        assertTrue(response.contains("[OK]"));
-//
+//    public void testInvalidCreate() throws IOException {
+////        server.dbStore.deleteEmptyDir("testDb");
+//        String database = "testDb";
+////        String validCr = sendCommandToServer("CREATE DATABASE " + database + ";");
+////        assertTrue(validCr.contains("[OK]"));
+//        //String invalidCr = "CREATE DATABASE " + database + ";";
+//        SyntaxException thrown = assertThrows(SyntaxException.class, () -> sendCommandToServer("CREATE DATABASE testDb;"));
+//        //System.out.println(thrown.getMessage());
+//        assertTrue(thrown.getMessage().contains("[ERROR]"));
+////        server.dbStore.deleteEmptyDir(database);
+////        assertNotEquals(database, server.dbStore.dbName);
+////        assertEquals(null, server.dbStore.dbName);
+////        assertEquals(null, server.dbStore.dbPath);
 //    }
 //
-//    // A test to make sure that querying returns a valid ID (this test also implicitly checks the "==" condition)
-//    // (these IDs are used to create relations between tables, so it is essential that suitable IDs are being generated and returned !)
-//    @Test
-//    public void testQueryID() {
-//        String randomName = generateRandomName();
-//        sendCommandToServer("CREATE DATABASE " + randomName + ";");
-//        sendCommandToServer("USE " + randomName + ";");
-//        sendCommandToServer("CREATE TABLE marks (name, mark, pass);");
-//        sendCommandToServer("INSERT INTO marks VALUES ('Simon', 65, TRUE);");
-//        String response = sendCommandToServer("SELECT id FROM marks WHERE name == 'Simon';");
-//        // Convert multi-lined responses into just a single line
-//        String singleLine = response.replace("\n"," ").trim();
-//        // Split the line on the space character
-//        String[] tokens = singleLine.split(" ");
-//        // Check that the very last token is a number (which should be the ID of the entry)
-//        String lastToken = tokens[tokens.length-1];
-//        try {
-//            Integer.parseInt(lastToken);
-//        } catch (NumberFormatException nfe) {
-//            fail("The last token returned by `SELECT id FROM marks WHERE name == 'Simon';` should have been an integer ID, but was " + lastToken);
-//        }
-//    }
+////    @Test
+////    public void testCreateDbOk() {
+////        String randomName = generateRandomName();
+////        String response = sendCommandToServer("CREATE DATABASE " + randomName + ";");
+////        assertTrue(response.contains("[OK]"));
+////    }
 //
-//    // A test to make sure that databases can be reopened after server restart
-//    @Test
-//    public void testTablePersistsAfterRestart() {
-//        String randomName = generateRandomName();
-//        sendCommandToServer("CREATE DATABASE " + randomName + ";");
-//        sendCommandToServer("USE " + randomName + ";");
-//        sendCommandToServer("CREATE TABLE marks (name, mark, pass);");
-//        sendCommandToServer("INSERT INTO marks VALUES ('Simon', 65, TRUE);");
-//        // Create a new server object
-//        server = new DBServer();
-//        sendCommandToServer("USE " + randomName + ";");
-//        String response = sendCommandToServer("SELECT * FROM marks;");
-//        assertTrue(response.contains("Simon"), "Simon was added to a table and the server restarted - but Simon was not returned by SELECT *");
-//    }
-//
-//    // Test to make sure that the [ERROR] tag is returned in the case of an error (and NOT the [OK] tag)
-//    @Test
-//    public void testForErrorTag() {
-//        String randomName = generateRandomName();
-//        sendCommandToServer("CREATE DATABASE " + randomName + ";");
-//        sendCommandToServer("USE " + randomName + ";");
-//        sendCommandToServer("CREATE TABLE marks (name, mark, pass);");
-//        sendCommandToServer("INSERT INTO marks VALUES ('Simon', 65, TRUE);");
-//        String response = sendCommandToServer("SELECT * FROM libraryfines;");
-//        assertTrue(response.contains("[ERROR]"), "An attempt was made to access a non-existent table, however an [ERROR] tag was not returned");
-//        assertFalse(response.contains("[OK]"), "An attempt was made to access a non-existent table, however an [OK] tag was returned");
-//    }
+////    @Disabled
+////    @Test
+////    public void testUseOk() {
+////        String randomName = "mihirTest";
+////        sendCommandToServer("CREATE DATABASE " + randomName + ";");
+////        String response = sendCommandToServer("USE " + randomName + ";");
+////        assertTrue(response.contains("[OK]"));
+////
+////    }
+////
+////    // A test to make sure that querying returns a valid ID (this test also implicitly checks the "==" condition)
+////    // (these IDs are used to create relations between tables, so it is essential that suitable IDs are being generated and returned !)
+////    @Test
+////    public void testQueryID() {
+////        String randomName = generateRandomName();
+////        sendCommandToServer("CREATE DATABASE " + randomName + ";");
+////        sendCommandToServer("USE " + randomName + ";");
+////        sendCommandToServer("CREATE TABLE marks (name, mark, pass);");
+////        sendCommandToServer("INSERT INTO marks VALUES ('Simon', 65, TRUE);");
+////        String response = sendCommandToServer("SELECT id FROM marks WHERE name == 'Simon';");
+////        // Convert multi-lined responses into just a single line
+////        String singleLine = response.replace("\n"," ").trim();
+////        // Split the line on the space character
+////        String[] tokens = singleLine.split(" ");
+////        // Check that the very last token is a number (which should be the ID of the entry)
+////        String lastToken = tokens[tokens.length-1];
+////        try {
+////            Integer.parseInt(lastToken);
+////        } catch (NumberFormatException nfe) {
+////            fail("The last token returned by `SELECT id FROM marks WHERE name == 'Simon';` should have been an integer ID, but was " + lastToken);
+////        }
+////    }
+////
+////    // A test to make sure that databases can be reopened after server restart
+////    @Test
+////    public void testTablePersistsAfterRestart() {
+////        String randomName = generateRandomName();
+////        sendCommandToServer("CREATE DATABASE " + randomName + ";");
+////        sendCommandToServer("USE " + randomName + ";");
+////        sendCommandToServer("CREATE TABLE marks (name, mark, pass);");
+////        sendCommandToServer("INSERT INTO marks VALUES ('Simon', 65, TRUE);");
+////        // Create a new server object
+////        server = new DBServer();
+////        sendCommandToServer("USE " + randomName + ";");
+////        String response = sendCommandToServer("SELECT * FROM marks;");
+////        assertTrue(response.contains("Simon"), "Simon was added to a table and the server restarted - but Simon was not returned by SELECT *");
+////    }
+////
+////    // Test to make sure that the [ERROR] tag is returned in the case of an error (and NOT the [OK] tag)
+////    @Test
+////    public void testForErrorTag() {
+////        String randomName = generateRandomName();
+////        sendCommandToServer("CREATE DATABASE " + randomName + ";");
+////        sendCommandToServer("USE " + randomName + ";");
+////        sendCommandToServer("CREATE TABLE marks (name, mark, pass);");
+////        sendCommandToServer("INSERT INTO marks VALUES ('Simon', 65, TRUE);");
+////        String response = sendCommandToServer("SELECT * FROM libraryfines;");
+////        assertTrue(response.contains("[ERROR]"), "An attempt was made to access a non-existent table, however an [ERROR] tag was not returned");
+////        assertFalse(response.contains("[OK]"), "An attempt was made to access a non-existent table, however an [OK] tag was returned");
+////    }
 
 }
