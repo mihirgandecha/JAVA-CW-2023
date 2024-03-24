26081134262327c02481518cb6f692682743422f
mihirgandecha <mihirgandecha@outlook.com>
2024-03-24 23:03:44 +0000

    Table class (first draft)

--- cw-db/src/main/java/edu/uob/DBCmnd/Table.java
+++ cw-db/src/main/java/edu/uob/DBCmnd/Table.java
@@ -1,8 +1,16 @@
 package edu.uob.DBCmnd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Table {
     public String tbName;
     public int index;
+    int rows;
+    int cols;
+    static int id;
+    private Map<String, String> foreignKeys;
+    //HashMap<Integer, Row> rows>
 
     public void Table(){
 
