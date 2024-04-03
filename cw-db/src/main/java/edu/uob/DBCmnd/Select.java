76f880bc02748379b82c81dea4150571443852fc
mihirgandecha <mihirgandecha@outlook.com>
2024-04-03 19:54:35 +0100

    Select execute: moved environment check into seperate method

--- cw-db/src/main/java/edu/uob/DBCmnd/Select.java
+++ cw-db/src/main/java/edu/uob/DBCmnd/Select.java
@@ -40,20 +40,7 @@ public class Select implements DBCmnd {
 
     @Override
     public String execute(Parser p) throws SyntaxException, IOException {
-        if(dbStore.currentDbPath == null){
-            throw new SyntaxException(" USE command not executed on current DB!");
-        }
-        Path withTbFile = Path.of(dbStore.currentDbPath + File.separator + tableName + dbStore.EXTENSION);
-        File tableFile = new File(withTbFile.toString());
-        if(!tableFile.exists()){
-            throw new SyntaxException(" " + tableFile + " does not exist in path: " + withTbFile);
-        }
-        if (dbStore.tbName == null){
-            dbStore.tbName = tableName;
-        }
-        if (dbStore.table == null){
-            dbStore.readTbFile(withTbFile);
-        }
+        checkThatDbAndTbExisting();
         ArrayList<String> output = new ArrayList<>();
         StringBuilder line = new StringBuilder();
         try {
@@ -82,5 +69,22 @@ public class Select implements DBCmnd {
             output.add(line.toString().trim());
         }
         return "[OK]\n" + String.join("\n", output);
+    }
+
+    private void checkThatDbAndTbExisting() throws SyntaxException, IOException {
+        if(dbStore.currentDbPath == null){
+            throw new SyntaxException(" USE command not executed on current DB!");
+        }
+        Path withTbFile = Path.of(dbStore.currentDbPath + File.separator + tableName + dbStore.EXTENSION);
+        File tableFile = new File(withTbFile.toString());
+        if(!tableFile.exists()){
+            throw new SyntaxException(" " + tableFile + " does not exist in path: " + withTbFile);
+        }
+        if (dbStore.tbName == null){
+            dbStore.tbName = tableName;
+        }
+        if (dbStore.table == null){
+            dbStore.readTbFile(withTbFile);
+        }
     }
 }
