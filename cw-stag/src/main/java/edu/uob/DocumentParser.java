94b8f6bde59018ce6ae02c5705c4f342d30008c3
mihirgandecha <mihirgandecha@outlook.com>
2024-04-25 20:13:51 +0100

    Reformatting Action -> GameAction

--- cw-stag/src/main/java/edu/uob/DocumentParser.java
+++ cw-stag/src/main/java/edu/uob/DocumentParser.java
@@ -4,21 +4,42 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 
 public class DocumentParser {
-    private final javax.xml.parsers.DocumentBuilder p;
-    private Element root;
-    private NodeList actions;
-    private Document parsedDocument;
 
-    public DocumentParser(String actionsFileString) throws IOException, SAXException, ParserConfigurationException {
-        this.p = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        this.parsedDocument = p.parse("config" + File.separator + actionsFileString);
-        this.root = this.parsedDocument.getDocumentElement();
-        this.actions = root.getChildNodes();
+    public DocumentParser(String actionsFileString) throws GameError {
+        setup(actionsFileString);
     }
+
+    private void setup(String actionsFileString) throws GameError {
+        try {
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document parsedDocument = builder.parse(actionsFileString);
+            parseDocument(parsedDocument);
+        } catch (Exception e) {
+            throw new GameError(e.getMessage());
+        }
+    }
+
+    private void parseDocument(Document document) {
+        Element root = document.getDocumentElement();
+        NodeList actionNodes = root.getElementsByTagName("action");
+        for (int i = 0; i < actionNodes.getLength(); i++) {
+            Element actionElement = (Element) actionNodes.item(i);
+            GameAction gameAction = parseGameAction(actionElement);
+            addGameActionToMap(gameAction);
+        }
+    }
+
+    private GameAction parseGameAction(Element actionElement) {
+        GameAction gameAction = new GameAction();
+
+        return gameAction;
+    }
+
 }
