ba8538e7434a8ad4f448c97a7c2e47220a2afc9e
mihirgandecha <mihirgandecha@outlook.com>
2024-04-26 12:51:05 +0100

    Initiate storing game actions

--- cw-stag/src/main/java/edu/uob/GameEngine.java
+++ cw-stag/src/main/java/edu/uob/GameEngine.java
@@ -5,23 +5,30 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class GameEngine {
-    private File entitiesFile;
-    private File actionsFile;
+    private String entitiesFile;
+    private String actionsFile;
     public Map<String, Location> map;
-    public DocumentParser p;
+    public HashMap<String, HashSet<GameAction>> gameActions;
 
     public GameEngine(File entitiesFile, File actionsFile) {
-        this.entitiesFile = entitiesFile;
-        this.actionsFile = actionsFile;
+        this.entitiesFile = entitiesFile.toString();
+        this.actionsFile = actionsFile.toString();
         this.map = processEntitiesFile();
+        this.gameActions = new HashMap<>();
     }
 
     private Map<String, Location> processEntitiesFile() {
-        GraphvizParser p = new GraphvizParser(this.entitiesFile.toString());
+        GraphvizParser p = new GraphvizParser(this.entitiesFile);
         return p.getGameMap();
     }
 
+    private HashMap<String, HashSet<GameAction>> processActionsFile() throws GameError {
+        DocumentParser p = new DocumentParser(this.actionsFile);
+        return p.getGameActions();
+    }
 }
