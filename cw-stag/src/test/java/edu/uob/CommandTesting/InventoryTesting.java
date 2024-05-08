ba8647e5e0b29aaa5e4c434471acc4b845c401e7
mihirgandecha <mihirgandecha@outlook.com>
2024-05-08 11:01:29 +0100

    Setup for all command test files

---
+++ cw-stag/src/test/java/edu/uob/CommandTesting/InventoryTesting.java
@@ -0,0 +1,45 @@
+package edu.uob.CommandTesting;
+
+import edu.uob.GameServer;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
+public class InventoryTesting {
+
+    private GameServer server;
+
+    // Create a new server _before_ every @Test
+    @BeforeEach
+    void setup() throws Exception {
+        File entitiesFile = Paths.get("config" + File.separator + "basic-entities.dot").toAbsolutePath().toFile();
+        File actionsFile = Paths.get("config" + File.separator + "basic-actions.xml").toAbsolutePath().toFile();
+        server = new GameServer(entitiesFile, actionsFile);
+    }
+
+    String sendCommandToServer(String command) {
+        // Try to send a command to the server - this call will timeout if it takes too long (in case the server enters an infinite loop)
+        return assertTimeoutPreemptively(Duration.ofMillis(10000000), () -> {
+                    return server.handleCommand(command);
+                },
+                "Server took too long to respond (probably stuck in an infinite loop)");
+    }
+
+    String randomiseCasing(String inFromGenerateRandomName) {
+        StringBuilder randomiseCaseForName = new StringBuilder();
+        Random random = new Random();
+        for (char c : inFromGenerateRandomName.toCharArray()) {
+            if (random.nextBoolean()) {
+                randomiseCaseForName.append(java.lang.Character.toUpperCase(c));
+            } else {
+                randomiseCaseForName.append(java.lang.Character.toLowerCase(c));
+            }
+        }
+        return randomiseCaseForName.toString();
+    }
+}
